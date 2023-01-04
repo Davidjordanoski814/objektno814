b7d23f52e58879a1b261cb42c9faf3747e0582d8
Davidjordanoski814 <jordanoski.david@uklo.edu.mk>
2023-01-04 23:56:54 +0100

    11ti vezbi

---
+++ Edinaesetti Vezbi/src/zadaca1/Glavna.java
@@ -0,0 +1,13 @@
+package zadaca1;
+
+import java.io.IOException;
+
+public class Glavna {
+
+    public static void main(String[] args) throws IOException {
+
+        Datoteka d = new Datoteka ("file.txt");
+        d.zapishi("Tekstot");
+        d.prochitaj();
+    }
+}
